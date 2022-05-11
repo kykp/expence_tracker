eb3a40f5b0037d9444da35689ca2b3e2a73b5b1e
kykp <kykp@yandex.ru>
2022-05-11 13:24:13 +0400

    added css visible delete button

--- src/App.js
+++ src/App.js
@@ -1,83 +1,84 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import { History } from "./components/History/History";
 
 function App() {
-  const [data, setData]= useState([{
-    id: 0,
-    name: "Cash",
-    value: 500,
-    income: true,
-  }, {
-    id: 1,
-    name: "Book",
-    value: -40,
-    income: false,
-  },{
-    id: 2,
-    name: "Camera",
-    value: -200,
-    income: false,
+  const [data, setData] = useState([
+    {
+      id: 0,
+      name: "Cash",
+      value: 500,
+      income: true,
+    },
+    {
+      id: 1,
+      name: "Book",
+      value: -40,
+      income: false,
+    },
+    {
+      id: 2,
+      name: "Camera",
+      value: -200,
+      income: false,
+    },
+  ]);
+
+  const [text, setText] = useState("");
+  const [value, setValue] = useState("");
+  const [balance, setBalance] = useState();
+  const [onExpense, setExpence] = useState();
+  const [onIncome, setIncom] = useState();
+
+  function summ() {
+    let currentBalance = 0;
+
+    data.forEach((el) => (currentBalance += el.value));
+    return currentBalance;
   }
-]);
 
-const [text, setText] = useState("");
-const [value, setValue] = useState("");
-const [balance, setBalance] = useState();
-const [onExpense, setExpence] = useState();
-const [onIncome, setIncom] = useState();
+  function handleExpense() {
+    let expense = 0;
 
-
-function summ () {
-  let currentBalance = 0;
-
-  data.forEach(el => currentBalance += el.value )
-  return currentBalance;
-}
-
-function handleExpense () {
-  let expense = 0; 
-  
-  data.forEach(el => el.value < 0 ? expense += el.value : null)
-  return expense;
-}
-function handleIncome () {
-  let income = 0;
-  data.forEach(el => el.value > 0 ? income += el.value : null)
-  return income;
-}
-
-const onDeleteItem = (element) =>{
-  console.log(element)
-}
-
-useEffect(() => {
-  setBalance(summ)
-  setExpence(handleExpense)
-  setIncom(handleIncome)
-},[])
-
-
-const handleSubmit = (event) => {
-  event.preventDefault();
-
-  let id = data.length +1;
-  let name = text;
-  let price = +value;
-  let income = false;
-  if (price > 0) {
-    income = true;
-    setIncom(onIncome + price);
-  } else {
-    income = false;
-    setExpence(onExpense + price);
+    data.forEach((el) => (el.value < 0 ? (expense += el.value) : null));
+    return expense;
   }
-  setBalance(balance + price);
-  setData([...data, {id: id, name: name, value: price, income: income}])
-}
+  function handleIncome() {
+    let income = 0;
+    data.forEach((el) => (el.value > 0 ? (income += el.value) : null));
+    return income;
+  }
+
+  const onDeleteItem = (element) => {
+    const newData = data.filter((el) => el.id !== element.id);
+    setData(newData);
+  };
+
+  useEffect(() => {
+    setBalance(summ);
+    setExpence(handleExpense);
+    setIncom(handleIncome);
+  }, []);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    let id = data.length + 1;
+    let name = text;
+    let price = +value;
+    let income = false;
+    if (price > 0) {
+      income = true;
+      setIncom(onIncome + price);
+    } else {
+      income = false;
+      setExpence(onExpense + price);
+    }
+    setBalance(balance + price);
+    setData([...data, { id: id, name: name, value: price, income: income }]);
+  };
 
   return (
     <div className="App container">
-      
       <div className="balance container">
         <h1>Expense Tracker</h1>
         <h3>Your Balance</h3>
@@ -85,47 +86,52 @@ const handleSubmit = (event) => {
       </div>
       <div className="total container">
         <div>
-          <h3 >Income</h3>
+          <h3>Income</h3>
           <p className="total__income">${onIncome}</p>
         </div>
         <div>
-          <h3 >Expense</h3>
+          <h3>Expense</h3>
           <p className="total__expense">${onExpense}</p>
         </div>
       </div>
 
-     <History data={data} onDeleteItem={onDeleteItem}/>
+      <History data={data} onDeleteItem={onDeleteItem} />
 
       <form className="transaction container">
         <h2>Add New Transaction</h2>
         <div className="transaction__input">
-          <label className="label" htmlFor="text">Text</label>
-          <input 
+          <label className="label" htmlFor="text">
+            Text
+          </label>
+          <input
             type="text"
             id="text"
             name="text"
             value={text}
             onChange={(event) => setText(event.target.value)}
-            placeholder="Input Text"/>
+            placeholder="Input Text"
+          />
         </div>
         <div className="transaction__input">
-          <label className="label" htmlFor="amount">Amount (negative - expense, positive - income)</label>
-          <input 
+          <label className="label" htmlFor="amount">
+            Amount (negative - expense, positive - income)
+          </label>
+          <input
             type="text"
             id="amount"
             name="amount"
             value={value}
             onChange={(event) => setValue(event.target.value)}
-            placeholder="Enter Amount"></input>
+            placeholder="Enter Amount"
+          ></input>
         </div>
-        <input 
-          className="transaction__submit" 
-          type="submit" 
+        <input
+          className="transaction__submit"
+          type="submit"
           value="Add Transaction"
-          onClick={handleSubmit}/>
+          onClick={handleSubmit}
+        />
       </form>
-
-      
     </div>
   );
 }
--- src/components/History/History.jsx
+++ src/components/History/History.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import {RiDeleteBin7Line} from "react-icons/ri"
 
 const History = ({data, onDeleteItem = Function.prototype}) => {
@@ -8,12 +7,16 @@ const History = ({data, onDeleteItem = Function.prototype}) => {
                 <h2>History</h2>
                 {data.map(el => {
                     return (
-                        <div key={el.id} 
+                        <div 
+                            key={el.id} 
                             className={el.income
                                 ? "history__item plus": "history__item minus"}>
                             <h3>{el.name}</h3>
                             <h3>{el.value}</h3>
-                            <RiDeleteBin7Line onClick={() => onDeleteItem(el)} className="history__button"/>
+                            <RiDeleteBin7Line 
+                                onClick={() => onDeleteItem(el)} 
+                                className="history__button"
+                                />
 
                         </div>
                     )
