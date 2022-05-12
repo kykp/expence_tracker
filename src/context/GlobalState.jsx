55a9368ffa21d3b0203cd9aff4984d27583f427d
kykp <kykp@yandex.ru>
2022-05-12 12:55:35 +0400

    added localstorage

--- src/context/GlobalState.jsx
+++ src/context/GlobalState.jsx
@@ -1,4 +1,4 @@
-import React, {createContext, useReducer} from "react";
+import React, {createContext, useReducer, useEffect} from "react";
 import AppReducer from "./AppReducer"
 
 // initial state
@@ -18,7 +18,13 @@ export const GlobalContext = createContext(initialState);
 // provider components
 
 export const GlobalProvider = ({children}) => {
-    const [state, dispatch] = useReducer(AppReducer, initialState);
+    const [state, dispatch] = useReducer(AppReducer, initialState, () => {
+        const localData = localStorage.getItem("state")
+        return localData ? JSON.parse(localData) : initialState;
+    });
+    useEffect(() => {
+        localStorage.setItem("state", JSON.stringify(state))
+    },[state])
 
     //Actions
     function deleteTransactions(id) {
